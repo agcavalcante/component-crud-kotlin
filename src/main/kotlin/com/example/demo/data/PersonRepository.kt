1457563d9cb047957734563f0f28fcd46f63fbeb
Gabriel Andrade Cavalcante <gabrielandradecavalcante@pop-os.localdomain>
2021-02-05 07:59:47 -0300

    Delete wrong version

--- src/main/kotlin/com/example/demo/data/PersonRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.data
-
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface PersonRepository: MongoRepository<Person, String> {
-    fun findOneById(id: ObjectId): Person
-    override fun deleteAll()
-}
